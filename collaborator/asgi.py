aa7bb4636d74e3ff8fa6de96565cbe47ec9454f0
Shahid <shahid04m@gmail.com>
2024-06-05 18:02:03 +0530

    daphne error resolved

--- collaborator/asgi.py
+++ collaborator/asgi.py
@@ -1,13 +1,13 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaborator.settings')
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from communication import routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaborator.settings')
-
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.ws_urlpatterns
